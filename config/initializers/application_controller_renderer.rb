130f307acbd7e94782ffc28fd49b49ee28574404
HAMABE HAYATO <be_dash1122@icloud.com>
2021-02-17 09:51:09 +0900

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'cf837f531e4165334ac0b1e3cbe5a7c30901d090a5dce5dc8d5cf3be9132b113e27b262b296524d8ed89928404b65641ce0cd6c38c4a2faee7ce8b3c9f4ee028'
